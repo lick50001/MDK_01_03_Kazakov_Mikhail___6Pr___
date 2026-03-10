ebb7b7673e04236efa7a9a74d57ae4b893b5116f
Shahzod0079 <toshmatovshahzod69@gmail.com>
2026-03-10 16:29:48 +0500

    Разработанная модель и репозиторий данных. Казаков

---
+++ app/src/main/java/com/example/notes_kazakov/datas/RepoNotes.java
@@ -0,0 +1,8 @@
+package com.example.notes_kazakov.datas;
+
+import java.util.ArrayList;
+import com.example.notes_kazakov.domains.models.Note;
+
+public class RepoNotes {
+    public static ArrayList<Note> Notes = new ArrayList<>();
+}
